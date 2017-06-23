da5d52ee2143039544dbe0a4aadc0d09b9ccface
Jean-Michel Trivi <jmtrivi@google.com>
2017-06-22 19:20:26 -0700

    Media volume: from 15 to 25 steps
    Test: play media, use all 25 vol steps and observe varying attenuation
    Bug: 32195657
    
    AMEND: Change it to 30
    Change-Id: I7ac87df0b2b15de3a7bb87e739bd88ef7b185236

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -18,6 +18,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.ras.enabled=false \
     ro.vendor.audio.sdk.ssr=false \
     ro.vendor.audio.sdk.fluencetype=none \
+    ro.config.media_vol_steps=30 \
     ro.qc.sdk.audio.fluencetype=none \
     ro.qc.sdk.audio.ssr=false \
     vendor.audio_hal.period_size=240 \
